a5c4c7994c46b5aba94da4823e511b270f33e296
perfectionist007 <turash.chowdhury@gmail.com>
2024-04-25 22:54:15 +0600

    first commit

---
+++ WebApiOne/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiOne.Entities;
+
+namespace WebApiOne.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) :base(options)
+        {
+            
+        }
+
+        public DbSet<SuperHero> SuperHeroes{ get; set; }
+    }
+}
